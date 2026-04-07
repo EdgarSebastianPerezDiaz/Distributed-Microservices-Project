74afe57c2e3c7df5622d8374aff161d90f8bf35f
Sebastian Perez <edgar.perez02@uptc.edu.co>
2026-04-07 08:37:45 -0500

    Clean main branch: move supplier and contract services to supplier-contract branch

--- contrato-service/contrato-service/src/main/java/com/distribuidos/contrato_service/model/ContractStatus.java
+++
@@ -1,9 +0,0 @@
-package com.distribuidos.contrato_service.model;
-
-public enum ContractStatus {
-    BORRADOR,      // Estado inicial - editable
-    ACTIVO,        // Contrato publicado y activo
-    EN_EJECUCION,  // En ejecución
-    VENCIDO,       // Finalizado por fecha
-    ANULADO        // Cancelado por ADMIN
-}
